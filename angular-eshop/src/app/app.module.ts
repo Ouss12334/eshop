a4dad937ed55cc2fd7190aa90eba67665c843047
Ouss12334 <turki.oussama.95@gmail.com>
2021-06-06 19:26:11 +0100

    add footer, suggestions menu,

--- angular-eshop/src/app/app.module.ts
+++ angular-eshop/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuComponent } from './menu/menu.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
 
 @NgModule({
   declarations: [
@@ -15,14 +17,15 @@ import { MenuComponent } from './menu/menu.component';
     TopBarComponent,
     ProductListComponent,
     FooterComponent,
-    MenuComponent
+    MenuComponent,
+    SuggestionsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      {path: '', component: ProductListComponent}
+      {path: '', component: SuggestionsComponent}
     ])
   ],
   providers: [],
